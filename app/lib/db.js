bdb8bf16460352bddb36454b43780c68d78b8e09
Mohammad Ragib <md.ragib@Mohammads-MacBook-Air.local>
2026-03-13 20:38:59 +0530

    db set in dotenv

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,11 +2,11 @@ import mysql from "mysql2/promise";
 
 
 export const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "newpassword",
-    database: "portfolio",
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 try {
     const connection = await db.getConnection();
