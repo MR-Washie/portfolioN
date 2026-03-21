d484a7d5e2421dfde6e72c1151a24886c8c0ea8e
Mohammad Ragib <mdragib.mth2005@gmail.com>
2026-03-21 16:33:46 +0530

    add project resolve

--- app/(pages)/project/page.jsx
+++ app/(pages)/project/page.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Project = async () => {
 
-  const [project] = await db.execute("select * from project order by created_at desc")
+  const [project] = await db.execute("select * from project")
 
 
   return (
